01f639c0fd919ebaca05edfa00d93fe9bdb464e1
Pamela Redjepovska <pamela.redjepovska@pixyle.ai>
2024-06-15 18:57:52 +0200

    Run in debug mode, remove GET method

--- src/main_api/routes/health_check.py
+++ src/main_api/routes/health_check.py
@@ -3,6 +3,6 @@ from flask import Blueprint, jsonify
 main_api = Blueprint("main_api", __name__)
 
 
-@main_api.route("/health", methods=["GET"])
+@main_api.route("/health")
 def health_check():
     return jsonify({"status": "Healthy"}), 200
