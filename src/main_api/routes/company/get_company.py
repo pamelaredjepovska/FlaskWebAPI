c3f92bfb6577322d478230cfabcf20f9f4c5e277
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-06-17 14:52:53 +0200

    Get company endpoint

---
+++ src/main_api/routes/company/get_company.py
@@ -0,0 +1,31 @@
+from flask import current_app, g, jsonify, request
+from psycopg import OperationalError, ProgrammingError
+
+from src import get_db_connection
+from src.main_api import main_api_blueprint as bp
+
+
+@bp.route("/company")
+def get_company():
+    try:
+        #  Only allow GET requests
+        if request.method != "GET":
+            return jsonify({"error": "Method not allowed"}), 405  # Method Not Allowed
+
+        db_pool = current_app.config["db_pool"]
+        query = "SELECT ARRAY_AGG(name) AS companies FROM company"
+        with get_db_connection(db_pool) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                companies = cursor.fetchone()
+
+        if not companies:
+            return jsonify({"error": "No companies found"}), 404  # Not Found
+
+        return companies
+
+    except (OperationalError, ProgrammingError) as e:
+        return jsonify({"error": str(e)}), 500  # Internal Server Error
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400  # Bad Request
