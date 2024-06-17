d0eb7e1be0cbb10a8d2e83843c3726a8b52893a3
Pamela Redjepovska <pamelaredjepovska@gmail.com>
2024-06-17 10:30:58 +0200

    Read swagger json contents from a file

--- src/swagger.py
+++ src/swagger.py
@@ -1,41 +1,25 @@
+import json
+
 from flask import jsonify
 
 
 def swagger_config(app):
     @app.route("/swagger")
     def swagger_spec():
-        return jsonify(
-            {
-                "swagger": "2.0",
-                "info": {
-                    "title": "My Flask API",
-                    "description": "API documentation with Swagger UI",
-                    "version": "1.0.0",
-                },
-                "host": "localhost:5000",
-                "basePath": "/main_api",
-                "schemes": ["http"],
-                "paths": {
-                    "/health": {
-                        "get": {
-                            "summary": "Health Check",
-                            "description": "Check the health status of the API",
-                            "responses": {
-                                "200": {
-                                    "description": "API is healthy",
-                                    "schema": {
-                                        "type": "object",
-                                        "properties": {
-                                            "status": {
-                                                "type": "string",
-                                                "example": "Healthy",
-                                            }
-                                        },
-                                    },
-                                }
-                            },
-                        }
-                    },
-                },
-            }
-        )
+        try:
+            json_file_path = "src/resources/swagger_config.json"
+
+            # Open the JSON file in read mode
+            with open(json_file_path, "r") as fp:
+                swagger_docs = json.load(fp)
+                # Return the JSON content with a 200 status code
+                return jsonify(swagger_docs[0]), 200
+        except FileNotFoundError:
+            # Handle file not found error
+            return jsonify({"error": "swagger_config.json file not found"}), 404
+        except json.JSONDecodeError:
+            # Handle JSON decoding error
+            return (
+                jsonify({"error": "Error decoding JSON from swagger_config.json"}),
+                500,
+            )
